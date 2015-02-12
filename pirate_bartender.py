23444b323f31d9935682eb1083b8313183a8f277
denrsch <denriquezsch@gmail.com>
2015-02-12 01:02:18 +0000

    pirate bartender draft

---
+++ pirate_bartender.py
@@ -0,0 +1,44 @@
+""""questions = {
+    "fruity": "are ye a fruity drink lubber?",
+    "smokey": "Or Do ye prefer something smokey, like mezcal or whisky?",
+    "salty": "would ye like something salty like the sea?",
+    "citrus": "would ye like a lime in that?"
+}""""""
+
+ingredients = {
+    "fruity": "splash of orange juice" 
+    "smokey": "shot of mezcal", "shot of whiskey"
+    "salty": "cup of lemonade", "glug of lime juice", "salt around the rim"
+    "citrus": "lemon juice", "grapefruit juice", "cranberry juice", "two shots of vodka", "slice of lime"
+   
+}"""
+
+fruity = raw_input('are ye a fruity drink lubber?')
+smokey = raw_input('Or Do ye prefer something smokey, like mezcal or whisky?')
+salty = raw_input('would ye like something salty like the sea?')
+citrus = raw_input('would ye like a lime in that?')
+
+#print "So, %r fruity, %r smokey, %r salty and %r citrus? Is that correct?" % (fruity, smokey, salty, citrus)
+
+def customer_drink_order():
+  for True in fruity:
+    print "we'll add a splash of orange juice"
+  else:
+    print "ok, we'll skip that."
+    
+  elif smokey == True:
+    print "I'll add a shot of mezcal and shot of whiskey."
+  else:
+    print "ok, we'll skip that."
+    
+  elif salty == True:
+    print "I'll add some lemonade and put salt around the rim."
+  else:
+    print "ok, we'll skip that."
+    
+  elif citrus == True:
+    print "I'll throw in a slice of lime."
+  else:
+    print "ok, we'll skip that."
+
+
---
+++ side_effect_test.py
@@ -0,0 +1,29 @@
+#def side_effect_test(value):
+    # Do something to modify the value
+   # value[1] = "orange"
+   # print "Inside the function, the value becomes {}".format(value)
+
+#if __name__ == "__main__":
+    # Create the value
+    #value = ["red", "green", "blue"]
+
+    #print "Outside the function, the value starts as {}".format(value)
+
+    #side_effect_test(value)
+
+    #print "Outside the function, the value is now {}".format(value)
+    
+def side_effect_test(value):
+    # Do something to modify the value
+    value[1] = "3"
+    print "Inside the function, the value becomes {}".format(value)
+
+if __name__ == "__main__":
+    # Create the value
+    value = [3, 4, 5]
+
+    print "Outside the function, the value starts as {}".format(value)
+
+    side_effect_test(value)
+
+    print "Outside the function, the value is now {}".format(value)
